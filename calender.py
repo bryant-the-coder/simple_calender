ac873b9dcc21f92cbed61418885c3489ca19f43a
bryant-the-coder <foobryant99@gmail.com>
2023-02-26 12:35:00 +0800

    push fix when user input value more than 12

--- calender.py
+++ calender.py
@@ -3,4 +3,8 @@ import calendar
 year = int(input("Please type in your desire year: "))
 month = int(input("Please type in your desire month: "))
 
+while month > 12:
+    print("That isnt a valid month")
+    month = int(input("Please retype in your desire month: "))
+
 print("\n", calendar.month(year, month))
